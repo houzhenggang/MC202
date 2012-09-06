54fe30e079643c8edd4bd31558143965636a5d8b
Pedro E. M. Brito <pedroembrito@gmail.com>
2012-09-06 13:02:53 -0300

    Lab03 commit inicial.

---
+++ lab03/principal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "grandeint.h"
+#include "balloc.h"
+
+int main()
+{
+	char op;
+
+	scanf("%c\n", &op);
+
+	switch (op) {
+		case ('+'):
+			soma();
+			break;
+		case ('*'):
+			multiplicacao();
+			break;
+		case ('/'):
+			divide();
+			break;
+		case ('G'):
+			godel();
+			break;
+		default:
+			printf("Operacao invalida\n");
+			break;
+	}
+	bapply(bprint);
+	return 0;
+}
