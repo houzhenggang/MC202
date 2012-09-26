178ccc44cb42d00421fb939df1d7cd7cb18cbe89
Pedro E. M. Brito <pedroembrito@gmail.com>
2012-09-26 19:54:28 -0300

    Wat do now?

--- lab04/principal.c
+++ lab04/principal.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 /*******************************************************************************
 Pedro Emílio Machado de Brito - RA 137264
@@ -29,59 +30,98 @@ void imprime(int mat[9][9]){
 	return;
 }
 
-int resolve(int mat[9][9], int i, int j)
+int resolve2(int mat[9][9], int whatever_a, int whatever_b)
 {
-  int percorre = 0, pilha[82][12] = {{0}}, pos = 0;
-  while (pos >= 0 && pos < 81)
+  int percorre, pilha[81][12] = {{0}}, pos = 0, max, i, j;
+  for (i = 0; i < 9; i++)
     {
+      for (j = 0; j < 9; j++)
+	{
+	  if (mat[i][j])
+	    continue;
+	  pilha[pos][10] = i;
+	  pilha[pos][11] = j;
+	  pos++;
+	}
+    }
+  max = pos;
+  pos = 0;
+  while (pos >= 0 && pos < max)
+    {
+      i = pilha[pos][10];
+      j = pilha[pos][11];
       if (!pilha[pos][0])
 	{
-	  do
-	    {
-	      do
-		{
-		  if (!mat[pilha[pos][10]][pilha[pos][11]])
-		    goto vazio;
-		  pilha[pos][11]++;
-		}
-	      while ( pilha[pos][11] < 9);
-	      pilha[pos][11] = 0;
-	      pilha[pos][10]++;
-	    }
-	  while (pilha[pos][10] < 9);
-	  if (pilha[pos][10] == 9)
-	    return 1;
-	vazio:
-	  for (percorre = 1; percorre < 10; percorre++)
-	    pilha[pos][percorre] = 0;
 	  for (percorre = 0; percorre < 9; percorre++)
 	    {
-	      pilha[pos][mat[pilha[pos][10]][percorre]] = 1;
-	      pilha[pos][mat[percorre][pilha[pos][11]]] = 1;
-	      pilha[pos][mat[pilha[pos][10]/3*3+percorre/3][pilha[pos][11]/3*3+percorre%3]] = 1;
+	      pilha[pos][mat[i][percorre]] = 1;
+	      pilha[pos][mat[percorre][j]] = 1;
+	      pilha[pos][mat[i-(i%3)+percorre/3][j-(j%3)+percorre%3]] = 1;
 	    }
 	}
-      while ( pilha[pos][0] < 10 )
+      while ( 1 )
 	{
 	  if (!pilha[pos][pilha[pos][0]])
 	    {
-	      mat[pilha[pos][10]][pilha[pos][11]] = pilha[pos][0];
-	      pilha[pos][0]++;
-	      pos++;
+	      pilha[pos][pilha[pos][0]] = 1;
+	      mat[i][j] = pilha[pos++][0];
 	      break;
 	    }
 	  pilha[pos][0]++;
-	}
-      if (pilha[pos][0] == 10)
-	{
-	  mat[pilha[pos][10]][pilha[pos][11]] = pilha[pos][0] = 0;
-	  pos--;
+	  if (pilha[pos][0] >= 10)
+	    {
+	      mat[i][j] = 0;
+	      memset(pilha[pos],0,10*sizeof(int));
+	      pos--;
+	      break;
+	    }
 	}
     }
-  if (pos < 0)
-    return 0;
-  else
+  if (pos == max)
     return 1;
+  return 0;
+}
+
+int resolve(int mat[9][9], int i, int j)
+{
+  int percorre = 0, usado[10] = {0};
+  do
+    {
+      do
+	{
+	  if (!mat[i][j])
+	    {
+	      percorre = 1;
+	      break;
+	    }
+	  j++;
+	}
+      while ( j < 9);
+      if (percorre)
+	break;
+      j = 0;
+      i++;
+    }
+  while (i < 9);
+  if (i == 9)
+    return 1;
+  for (percorre = 0; percorre < 9; percorre++)
+    {
+      usado[mat[i][percorre]] = 1;
+      usado[mat[percorre][j]] = 1;
+      usado[mat[i/3*3+percorre/3][j/3*3+percorre%3]] = 1;
+    }
+  for (percorre = 1; percorre < 10; percorre++)
+    {
+      if (!usado[percorre])
+	{
+	  mat[i][j] = percorre;
+	  if (resolve(mat, i, j))
+	    return 1;
+	}
+    }
+  mat[i][j] = 0;
+  return 0;
 }
 
 int sudoku(int mat[9][9])
@@ -94,7 +134,7 @@ int sudoku(int mat[9][9])
       mat[x-1][y-1] = v;
     }
   /* chamada inicia a recursão */
-  return resolve(mat, 0, 0);
+  return resolve2(mat, 0, 0);
 }
 
 int main()
