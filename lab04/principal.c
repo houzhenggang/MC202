a16d9eac0ddca6d7783f184aae1102dc585d4296
Pedro E. M. Brito <pedroembrito@gmail.com>
2012-09-26 16:28:13 -0300

    Muito sucesso

--- lab04/principal.c
+++ lab04/principal.c
@@ -31,61 +31,57 @@ void imprime(int mat[9][9]){
 
 int resolve(int mat[9][9], int i, int j)
 {
-  int percorre = 0, usado[10] = {0};
-  /* loop que procura o próximo dígito a ser determinado */
-  do
+  int percorre = 0, pilha[82][12] = {{0}}, pos = 0;
+  while (pos >= 0 && pos < 81)
     {
-      do
+      if (!pilha[pos][0])
 	{
-	  /* achou dígito zerado */
-	  if (!mat[i][j])
+	  do
 	    {
-	      /* usa "percorre" como flag pra sair do loop aninhado */
-	      percorre = 1;
+	      do
+		{
+		  if (!mat[pilha[pos][10]][pilha[pos][11]])
+		    goto vazio;
+		  pilha[pos][11]++;
+		}
+	      while ( pilha[pos][11] < 9);
+	      pilha[pos][11] = 0;
+	      pilha[pos][10]++;
+	    }
+	  while (pilha[pos][10] < 9);
+	  if (pilha[pos][10] == 9)
+	    return 1;
+	vazio:
+	  for (percorre = 1; percorre < 10; percorre++)
+	    pilha[pos][percorre] = 0;
+	  for (percorre = 0; percorre < 9; percorre++)
+	    {
+	      pilha[pos][mat[pilha[pos][10]][percorre]] = 1;
+	      pilha[pos][mat[percorre][pilha[pos][11]]] = 1;
+	      pilha[pos][mat[pilha[pos][10]/3*3+percorre/3][pilha[pos][11]/3*3+percorre%3]] = 1;
+	    }
+	}
+      while ( pilha[pos][0] < 10 )
+	{
+	  if (!pilha[pos][pilha[pos][0]])
+	    {
+	      mat[pilha[pos][10]][pilha[pos][11]] = pilha[pos][0];
+	      pilha[pos][0]++;
+	      pos++;
 	      break;
 	    }
-	  /* avança coluna */
-	  (j)++;
+	  pilha[pos][0]++;
 	}
-      while ( j < 9);
-      if (percorre)
-	break;
-      /* reseta coluna e avança linha */
-      j = 0;
-      i++;
-    }
-  while (i < 9);
-  /* se chegamos ao fim do tabuleiro (caso básico), uma solução foi encontrada */
-  if (i == 9)
-    return 1;
-  /* elimina os dígitos já usados na linha, coluna e quadrado 3x3 */
-  for (percorre = 0; percorre < 9; percorre++)
-    {
-      /* percorre linha */
-      usado[mat[i][percorre]] = 1;
-      /* percorre coluna */
-      usado[mat[percorre][j]] = 1;
-      /* percorre quadrado 3x3 */
-      usado[mat[i/3*3+percorre/3][j/3*3+percorre%3]] = 1;
-    }
-  /* agora usado[x] == 0 se x é um dígito válido para essa posição */
-  /* loop testa cada um dos dígitos e chama a recursão para o restante do tabuleiro */
-  for (percorre = 1; percorre < 10; percorre++)
-    {
-      if (!usado[percorre])
+      if (pilha[pos][0] == 10)
 	{
-	  /* põe o dígito na posição */
-	  mat[i][j] = percorre;
-	  /* chama a recursão para o restante do tabuleiro */
-	  if (resolve(mat, i, j))
-	    /* se é solução retorna imediatamente */
-	    return 1;
+	  mat[pilha[pos][10]][pilha[pos][11]] = pilha[pos][0] = 0;
+	  pos--;
 	}
     }
-  /* se chegamos aqui, não há solução para o estado atual do tabuleiro */
-  /* a posição atual do tabuleiro volta ao valor inicial e retorna falha */
-  mat[i][j] = 0;
-  return 0;
+  if (pos < 0)
+    return 0;
+  else
+    return 1;
 }
 
 int sudoku(int mat[9][9])
